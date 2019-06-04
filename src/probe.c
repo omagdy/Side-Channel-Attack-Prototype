494e9e0a9203cb7db05b015df8933c9598b0fce4
Omar Hussein <omar.hussein1>
2019-06-04 18:14:15 +0200

    Removed timing between loops

--- src/probe.c
+++ src/probe.c
@@ -12,72 +12,111 @@
 #define TIME_LOOP_THRESHOLD (10000000000)	// Microseconds
 #define LOOP_WAIT (5)
 
-void write_to_csv(int* p_time, uint64_t* l_time_1, uint64_t* l_time_2, int array_size, char* fname){
+// void write_to_csv(int* p_time, uint64_t* l_time_1, uint64_t* l_time_2, int array_size, char* fname){
 
-    // To use variable filename
-    char buf[0x100];
-    snprintf(buf, sizeof(buf), "%s.csv", fname);
+//     // To use variable filename
+//     char buf[0x100];
+//     snprintf(buf, sizeof(buf), "%s.csv", fname);
 
-    FILE *fp = fopen(buf, "w");
+//     FILE *fp = fopen(buf, "w");
 
-    for(int i = 0; i < array_size; i++){
-        fprintf(fp, "%d,%lu,%lu\n", *p_time, *l_time_1, *l_time_2);
-        p_time++;
-        l_time_1++;
-        l_time_2++;
+//     for(int i = 0; i < array_size; i++){
+//         fprintf(fp, "%d,%lu,%lu\n", *p_time, *l_time_1, *l_time_2);
+//         p_time++;
+//         l_time_1++;
+//         l_time_2++;
+//     }
+
+//     fclose (fp);
+// } 
+
+
+// void task_2_a(){
+
+//     int index1 = 5, index2 = 7; // Indexes of the numbers variable to probe
+//     int sleep_duration = 10; // sleep duration
+
+//     uint64_t load_time_array_1[ITERATION_COUNT], load_time_array_2[ITERATION_COUNT];
+//     int probe_time_array[ITERATION_COUNT];
+
+//     // Pointers to save numbers index addresses
+//     const void *num_addr1;
+//     const void *num_addr2;
+
+//     for (int i = 0; i < ITERATION_COUNT; ++i)
+//     {
+
+//         // Flush and reload first index
+//         num_addr1 = numbers[index1];
+//         load_time_array_1[i] = reload(num_addr1);
+
+//         // Flush and reload second index
+//         num_addr2 = numbers[index2];
+//         load_time_array_2[i] = reload(num_addr2);
+
+//         // Flush both indexes
+//         flush(num_addr1);
+//         flush(num_addr2);
+
+//         probe_time_array[i] = sleep_duration * i;
+
+//         // usleep(sleep_duration);
+//     }
+
+// 	write_to_csv(probe_time_array, load_time_array_1, load_time_array_2, ITERATION_COUNT, "data");
+
+// 	return;
+// }
+
+
+void print_results_to_file(char *nme, uint64_t *arr){
+	int j=0;
+	char snum[50];
+	FILE *fp;
+	fp = fopen(nme, "w+");
+    for(j = 0; j < ITERATION_COUNT; j++) {
+		sprintf(snum, "%ld",arr[j]);
+		strcat(snum, " ");
+		fputs(snum, fp);
     }
-
-    fclose (fp);
-} 
+    fputs("\n", fp);
+    for(j = 0; j < ITERATION_COUNT; j++) {
+		sprintf(snum, "%d",j+1);
+		strcat(snum, " ");
+    	fputs(snum, fp);
+    }
+    fclose(fp);
+}
 
 
 void task_2_a(){
 
-    int index1 = 5, index2 = 7; // Indexes of the numbers variable to probe
-    int sleep_duration = 10; // sleep duration
+	uint64_t pr_5_time[ITERATION_COUNT];
+	uint64_t pr_7_time[ITERATION_COUNT];
+	// ptr5 = (char *)malloc(sizeof(numbers[5]));
+	// ptr5 = numbers[5];
+	// pr_7_time[i] = reload(ptr7);
+	// ptr7 = (char *)malloc(sizeof(numbers[7]));
+	// ptr7 = numbers[7];
 
-    uint64_t load_time_array_1[ITERATION_COUNT], load_time_array_2[ITERATION_COUNT];
-    int probe_time_array[ITERATION_COUNT];
-
-    // Pointers to save numbers index addresses
-    const void *num_addr1;
-    const void *num_addr2;
-
-    for (int i = 0; i < ITERATION_COUNT; ++i)
-    {
-
-        // Flush and reload first index
-        num_addr1 = numbers[index1];
-        load_time_array_1[i] = reload(num_addr1);
-
-        // Flush and reload second index
-        num_addr2 = numbers[index2];
-        load_time_array_2[i] = reload(num_addr2);
-
-        // Flush both indexes
-        flush(num_addr1);
-        flush(num_addr2);
-
-        probe_time_array[i] = sleep_duration * i;
-
-        usleep(sleep_duration);
-    }
-
-	write_to_csv(probe_time_array, load_time_array_1, load_time_array_2, ITERATION_COUNT, "data");
-
+	for(int i=0; i<ITERATION_COUNT; i++){
+		pr_7_time[i] = reload(numbers[7]);
+		pr_5_time[i] = reload(numbers[5]);
+		flush(numbers[7]);
+		flush(numbers[5]);
+		usleep(2);
+	}
+	print_results_to_file("Probe-numbers[5].csv", pr_5_time);
+	print_results_to_file("Probe-numbers[7].csv", pr_7_time);
 	return;
 }
+
+
 
 void task_2_b(){
 	
     int LOOPFOREVER = 1;
     int numbers_size = 11; // numbers variable indexes to check
-
-    // time for access loop
-    struct timespec start, end;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    uint64_t delta_us;
-    int time_flag=1;
 
     // Perform actual flush and reload on numbers variable
     const void *numbers_addr;
@@ -85,44 +124,24 @@ void task_2_b(){
     int access_detected = 0;
 
     // Determine detection threshold
-    int DETECTION_THRESHOLD;
+    int DETECTION_THRESHOLD = determing_tsc_threshold();
 
-    while(LOOPFOREVER)
-    {
-        DETECTION_THRESHOLD = determing_tsc_threshold();
-        while(time_flag)
-        {
+    while(LOOPFOREVER){
             for (int i = 0; i < numbers_size; ++i)
             {
-
                 numbers_addr = numbers[i+1];
                 number_access_time = flush_and_reload(numbers_addr);
 
                 if(number_access_time < DETECTION_THRESHOLD){
-                    // printf("Access for [%d], Time:%lu\n", i, number_access_time);
                     access_detected = 1;
                     break;// exit loop if any of the numbers array indexes was detected to be cached
                 }
                 // printf("Access Time for %d : %lu\n", i, number_access_time);
             }
-
             if(access_detected){
                 printf("Access\n");
             }
             access_detected = 0;
-
-            // Time counter for second while loop
-            clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-            delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000; //microseconds
-            if(delta_us>TIME_LOOP_THRESHOLD){
-                time_flag=0;
-            }
-
-        }
-
-        time_flag=1;
-        usleep(LOOP_WAIT);
-
     }
 }
 
--- src/probe.h
+++ src/probe.h
@@ -14,49 +14,63 @@
 # include <x86intrin.h>
 #endif
 
-// Comment for Omar, Now this is behaving as a limit that we need to manually set sometimes..
 #define TSC_THRESHOLD_UPPER_LIMIT (50)		// Added to Average cache hit to calculate TSC Threshold
 #define NUMBER_OF_ELEMENTS_IN_ARRAY (11)	// Size of numbers[] which we cache attack
 
-// Ahmer's Version
-uint64_t inline __attribute__((always_inline)) read_tsc() {
-	_mm_lfence();  // optionally wait for earlier insns to retire before reading the clock
-	uint64_t tsc = __rdtsc();
-	_mm_lfence();  // optionally block later instructions until rdtsc retires
-	return tsc;
-}
 
-// Ahmer's Version
-uint64_t inline __attribute__((always_inline)) read_tscp() {
-	unsigned dummy;
-	return __rdtscp(&dummy);  // waits for earlier insns to retire, but allows later to start
-}
+volatile uint64_t junk_first_time_miss;		// First time cache fetch will be very long
 
-// Ahmer's Version
-// void inline __attribute__((always_inline)) flush(const void* addr) {
-// 	asm volatile ("clflush 0(%0)\n"
-// 		:
-// 		: "c" (addr)
-// 		: "rax");
-// }
 
-// Omar's version
+
 inline __attribute__((always_inline)) void flush(const void *addr){
 	asm volatile("mfence" ::: "memory");
 	asm volatile ("clflush (%0)" :: "r"(addr));
 	asm volatile("mfence" ::: "memory");
 }
 
-// Ahmer's Version
-uint64_t inline __attribute__((always_inline)) reload(const void* addr) {
-	uint64_t t1;
 
-	t1 = read_tsc();
-	(void) *(volatile int*) addr;
-	return read_tsc() - t1;
+inline __attribute__((always_inline)) uint64_t reload(const void *addr){
+	// uint64_t total_time, over_head_time;
+	long long cycles_low, cycles_high, cycles_low1, cycles_high1, start, end;
+	// long long ocycles_low, ocycles_high, ocycles_low1, ocycles_high1, ostart, oend;
+	asm   volatile ("LFENCE\n\t"           
+					"RDTSC\n\t"
+					"mov %%rdx, %0\n\t"
+					"mov %%rax, %1\n\t"
+					: "=r" (cycles_high), "=r" (cycles_low):: "%rax", "%rbx", "%rcx", "%rdx", "memory");
+	asm volatile("lfence" ::: "memory"); 
+	(void) *(volatile int*)addr;
+	asm volatile("lfence" ::: "memory"); 
+	asm   volatile("RDTSCP\n\t"
+					"mov %%rdx, %0\n\t"
+					"mov %%rax, %1\n\t"
+					"LFENCE\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax", "%rbx", "%rcx", "%rdx", "memory"); 
+	start = ( (cycles_high << 32) | cycles_low );
+	end = ( (cycles_high1 << 32) | cycles_low1 );
+	return (uint64_t)(end-start);
+	// total_time = (uint64_t)(end-start);
+	// asm   volatile ("LFENCE\n\t"           
+	// 				"RDTSC\n\t"
+	// 				"mov %%rdx, %0\n\t"
+	// 				"mov %%rax, %1\n\t"
+	// 				: "=r" (ocycles_high), "=r" (ocycles_low):: "%rax", "%rbx", "%rcx", "%rdx", "memory"); 
+	// asm volatile("lfence" ::: "memory"); 
+	// asm volatile("lfence" ::: "memory"); 
+	// asm   volatile("RDTSCP\n\t"
+	// 				"mov %%rdx, %0\n\t"
+	// 				"mov %%rax, %1\n\t"
+	// 				"CPUID\n\t": "=r" (ocycles_high1), "=r" (ocycles_low1):: "%rax", "%rbx", "%rcx", "%rdx", "memory"); 
+
+	// ostart = ( (ocycles_high << 32) | ocycles_low );
+	// oend = ( ( ocycles_high1 << 32) | ocycles_low1 );
+	// over_head_time = (uint64_t)(oend-ostart);
+
+
+	// return total_time-over_head_time;
 }
 
-// Ahmer's Version
+
+
 uint64_t inline __attribute__((always_inline)) flush_and_reload(const void* addr) {
 	uint64_t time;
 
@@ -66,14 +80,12 @@ uint64_t inline __attribute__((always_inline)) flush_and_reload(const void* addr
 }
 
 
-volatile uint64_t junk_first_time_miss;		// First time cache fetch will be very long
-
 // This is not an inline function, it should not be in header file
 uint64_t determing_tsc_threshold(){
 
 	const void* addr;
 
-	int i = 1000;
+	int j = 1000;
 	uint64_t avg_time_cache_hit=0;//, avg_time_cache_miss=0;
 	junk_first_time_miss = 0;
 
@@ -82,13 +94,13 @@ uint64_t determing_tsc_threshold(){
 		junk_first_time_miss = junk_first_time_miss+reload(addr);
 	}
 
-	while(i>0){
+	while(j>0){
 		for(int i =0;i<NUMBER_OF_ELEMENTS_IN_ARRAY;i++){
 			addr = numbers[i];
 			avg_time_cache_hit = avg_time_cache_hit+reload(addr);
 		}
 
-		i--;
+		j--;
 	}
 
 	for(int i =0;i<NUMBER_OF_ELEMENTS_IN_ARRAY;i++){
@@ -99,50 +111,6 @@ uint64_t determing_tsc_threshold(){
 	return ((avg_time_cache_hit)/12000) + TSC_THRESHOLD_UPPER_LIMIT;
 }
 
-// inline __attribute__((always_inline)) void flush(const number_t *addr){
-// 	asm volatile("mfence" ::: "memory");
-// 	asm volatile ("clflush (%0)" :: "r"(addr));
-// 	asm volatile("mfence" ::: "memory");
-// }
 
 
-// inline __attribute__((always_inline)) uint64_t reload(const number_t *addr){
-// 	// uint64_t total_time, over_head_time;
-// 	long long cycles_low, cycles_high, cycles_low1, cycles_high1, start, end;
-// 	// long long ocycles_low, ocycles_high, ocycles_low1, ocycles_high1, ostart, oend;
-// 	asm   volatile ("LFENCE\n\t"           
-// 					"RDTSC\n\t"
-// 					"mov %%rdx, %0\n\t"
-// 					"mov %%rax, %1\n\t"
-// 					: "=r" (cycles_high), "=r" (cycles_low):: "%rax", "%rbx", "%rcx", "%rdx", "memory");
-// 	asm volatile("lfence" ::: "memory"); 
-// 	(void) *(volatile int*)addr;
-// 	asm volatile("lfence" ::: "memory"); 
-// 	asm   volatile("RDTSCP\n\t"
-// 					"mov %%rdx, %0\n\t"
-// 					"mov %%rax, %1\n\t"
-// 					"LFENCE\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax", "%rbx", "%rcx", "%rdx", "memory"); 
-// 	start = ( (cycles_high << 32) | cycles_low );
-// 	end = ( (cycles_high1 << 32) | cycles_low1 );
-// 	return (uint64_t)(end-start);
-// 	// total_time = (uint64_t)(end-start);
-// 	// asm   volatile ("LFENCE\n\t"           
-// 	// 				"RDTSC\n\t"
-// 	// 				"mov %%rdx, %0\n\t"
-// 	// 				"mov %%rax, %1\n\t"
-// 	// 				: "=r" (ocycles_high), "=r" (ocycles_low):: "%rax", "%rbx", "%rcx", "%rdx", "memory"); 
-// 	// asm volatile("lfence" ::: "memory"); 
-// 	// asm volatile("lfence" ::: "memory"); 
-// 	// asm   volatile("RDTSCP\n\t"
-// 	// 				"mov %%rdx, %0\n\t"
-// 	// 				"mov %%rax, %1\n\t"
-// 	// 				"CPUID\n\t": "=r" (ocycles_high1), "=r" (ocycles_low1):: "%rax", "%rbx", "%rcx", "%rdx", "memory"); 
-
-// 	// ostart = ( (ocycles_high << 32) | ocycles_low );
-// 	// oend = ( ( ocycles_high1 << 32) | ocycles_low1 );
-// 	// over_head_time = (uint64_t)(oend-ostart);
-
-
-// 	// return total_time-over_head_time;
-// }
 
